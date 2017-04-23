0130a0a6da6df37dbad64c3f842ec12e4a095c43
Slamjack <murlouk@gmail.com>
2017-04-23 17:33:43 -0400

    Turret.v2
    New Turret and Projectile

---
+++ Assets/Scripts/Enemies/TurretFire.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script used to
+/// </summary>
+public class TurretFire : MonoBehaviour {
+
+    [Header("References")]
+    #region References
+    [SerializeField]
+    private Transform TurretGunOutputTransform;
+    [SerializeField]
+    private Transform TEST_CurrentPlayerTransform;
+    #endregion
+
+    public void Cmd_Shoot()
+    {
+        Vector3 targetPosition = TEST_CurrentPlayerTransform.position;
+
+    }
+
+}
---
+++ Assets/Scripts/Enemies/TurretProjectileMain.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretProjectileMain : MonoBehaviour {
+
+    [Header("References")]
+    #region References
+    [SerializeField]
+    private Transform ProjectileTransfom;
+    #endregion
+
+    [Header("Properties")]
+    #region Properties
+    [SerializeField]
+    private float Speed = 0.1f;
+    [SerializeField]
+    private float LifeSpan = 5.0f;
+    #endregion
+
+    private float ExpireTime;
+    public Vector3 Velocity;
+
+    void Start()
+    {
+        ExpireTime = Time.time + LifeSpan;
+        StartCoroutine(CheckForExpiry());
+    }
+
+    void Update()
+    {
+        MoveProjectile();
+    }
+
+    void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+
+    void MoveProjectile()
+    {
+        ProjectileTransfom.position = ProjectileTransfom.position + Velocity * Time.deltaTime;
+    }
+
+    IEnumerator CheckForExpiry()
+    {
+        while (true)
+        {
+            if (Time.time > ExpireTime)
+            {
+                Destroy(gameObject);
+            }
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+}
