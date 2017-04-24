e052b4765da4d09ae91464b433ee649815923305
Hugo Delahousse <hugo.delahousse@gmail.com>
2017-04-23 20:34:07 -0400

    Naded and shit

---
+++ Assets/Scripts/Enemies/TeslaScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeslaScript : MonoBehaviour {
+
+    [SerializeField]
+    bool attacking;
+    bool wasAttacking = false;
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!wasAttacking && attacking)
+            anim.SetTrigger("Attack");
+        wasAttacking = attacking;
+
+    }
+
+    public void Fire()
+    { }
+}
